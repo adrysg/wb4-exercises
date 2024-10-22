2b7aa1667d27f5a0b163f85f0b84569502d21019
agabin <agabin@appdev.yearup.org>
2024-10-22 11:07:12 -0400

    update

--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -2,49 +2,64 @@ package com.pluralsight;
 
 public class Reservation {
 
-    public double kingRoom = 139;
-    public double doubleRoom = 124;
-    String getRoomType;
-    public double getPrice;
-    public int numberOfNights;
-    public boolean isWeekend;
-    public double reservationTotal;
+    private String roomType; //room type: king or double
+
+    private int numberOfNights;
+
+    private boolean isWeekend;
 
 
-    public Reservation(String getRoom, double getPrice, int numberOfNights, boolean isWeekend, double reservationTotal){
-        this.getRoomType = getRoomType;
-        this.getPrice = getPrice;
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend){
+        this.roomType = roomType;
         this.numberOfNights = numberOfNights;
         this.isWeekend = isWeekend;
-        this.reservationTotal = reservationTotal;
     }
 
-    public double getKingRoom() {
-        return kingRoom;
+    public String getRoomType(){
+        return roomType;
     }
 
-    public double getDoubleRoom() {
-        return doubleRoom;
+    public void setRoomType(String roomType){
+        this.roomType = roomType;
     }
 
-
-    public double getGetPrice() {
-        return getPrice;
+    public double getPrice(){
+        double price = 0;
+        if (this.roomType.equalsIgnoreCase("king")){
+            price = 139.0;
+        }
+        else if (this.roomType.equalsIgnoreCase("double")){
+            price = 124.0;
+        }
+        else{
+            System.out.println("Invalid room type, please try again.");
+        }
+        if (this.isWeekend){
+            price *= 1.1;
+        }
+        return price;
     }
 
     public int getNumberOfNights() {
         return numberOfNights;
     }
 
+    public void setNumberOfNights(int numberOfNights){
+        this.numberOfNights = numberOfNights;
+    }
+
     public boolean isWeekend() {
         return isWeekend;
     }
 
-    public double getReservationTotal() {
-        return reservationTotal;
+    public void setWeekend (boolean isWeekend){
+        this.isWeekend = isWeekend;
     }
 
-    public getRoomType(){
-
+    public double getReservationTotal(){
+        return this.getPrice() * this.numberOfNights;
     }
+
+
+
 }
